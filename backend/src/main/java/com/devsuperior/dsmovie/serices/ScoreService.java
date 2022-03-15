e72f7a7c958c57774ada9858528e3e2ab6cc64a4
lucascavalcante25 <52172991+lucascavalcante25@users.noreply.github.com>
2022-03-15 11:05:41 -0300

    save score

---
+++ backend/src/main/java/com/devsuperior/dsmovie/serices/ScoreService.java
@@ -0,0 +1,68 @@
+package com.devsuperior.dsmovie.serices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.dto.ScoreDTO;
+import com.devsuperior.dsmovie.entities.Movie;
+import com.devsuperior.dsmovie.entities.Score;
+import com.devsuperior.dsmovie.entities.User;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.repositories.ScoreRepository;
+import com.devsuperior.dsmovie.repositories.UserRepository;
+
+@Service
+public class ScoreService {
+
+	@Autowired
+	private MovieRepository movieRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private ScoreRepository scoreRepository;
+
+	@Transactional
+	public MovieDTO saveScore(ScoreDTO dto) {
+		
+		//Salvando um usuário
+		User user = userRepository.findByEmail(dto.getEmail());
+		if (user == null) {
+			user = new User();
+			user.setEmail(dto.getEmail());
+			user = userRepository.saveAndFlush(user);
+		}
+		
+		//Buscando um filme
+		Movie movie = movieRepository.findById(dto.getMovieId()).get();
+
+		//Salvando um score
+		Score score = new Score();
+		score.setMovie(movie);
+		score.setUser(user);
+		score.setValue(dto.getScore());
+		
+		score = scoreRepository.saveAndFlush(score);
+		
+		//Soma dos scores do filme
+		Double sum = 0.0;
+		for(Score s : movie.getScores()) {
+			sum = sum + s.getValue();
+		}
+		
+		//Calculando a média dos scores
+		Double avg = sum / movie.getScores().size();
+		
+		//Salvando um filme
+		movie.setScore(avg);
+		movie.setCount(movie.getScores().size());
+		
+		movie = movieRepository.save(movie);
+		
+		return new MovieDTO(movie);
+	}
+
+}
